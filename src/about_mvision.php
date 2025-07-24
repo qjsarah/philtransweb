c4811dc9ed1ff97b2b2af15663d19d55dbf0aae2
Chrischan-Chu <tanchristian2001@gmail.com>
2025-07-24 16:42:29 +0800

    About and MVision Desktop Done

---
+++ src/about_mvision.php
@@ -0,0 +1,62 @@
+<link rel="stylesheet" href="main.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+
+<body>
+   <section class="d-flex align-items-center text-white" 
+   style="background-image: url('../images/about_background.png'); 
+   background-size: contain; 
+   background-position: center; 
+   background-repeat: no-repeat;
+   height: 1000px;">
+   
+<div class="position-absolute top-0 end-0 mt-4 me-5 pt-2 fw-bold fs-1">
+      ABOUT US
+</div>
+        <div class="col-lg-6 ms-5">
+          <h2 class="fw-bold">PTAS: REVOLUTIONIZING RIDES AND REDEFINING THE TRYCICLE INDUSTRY</h2>
+          <p class="mt-4 fs-3">
+            In the ever-evolving landscape of transportation, PTAS emerges as more than just another app. 
+            It shatters the mold of traditional ride-hailing services, offering a revolutionary approach centered around the very people who keep the tricycle industry moving – the drivers. 
+            PTAS transcends the mere act of getting you from point A to point B; it's a catalyst for positive change, empowering drivers, enhancing passenger experiences.
+          </p>
+        </div>
+  </section>
+
+ <section class="container py-5 text-black">
+  <!-- MISSION Section -->
+  <div class="row align-items-center" style="margin-bottom:200px;" >
+    <div class="col-md-5 text-center">
+      <img src="../images/mission.png" alt="Mission Image" class="img-fluid" style="max-width: 400px;">
+    </div>
+    <div class="col-md-7">
+      <h1 class="text-danger fw-bold">MISSION</h1>
+      <p class="mt-4 fs-3">
+        Our mission is to empower every tricycle driver in the Philippines with cutting-edge technology, transforming their profession and boosting their income.
+        By embracing innovation, we aim to elevate the national transportation system while preserving the iconic tricycle experience.
+        Our commitment extends to passenger comfort, ensuring a smoother and more enjoyable journey for all Filipinos.
+      </p>
+    </div>
+  </div>
+  
+  <!-- VISION Section -->
+  <div class="row align-items-center flex-md-row-reverse" style="margin-bottom:150px;">
+    <div class="col-md-5 text-center">
+      <img src="../images/vision.png" alt="Vision Image" class="img-fluid" style="max-width: 400px;">
+    </div>
+    <div class="col-md-7 text-md-end">
+      <h1 class="text-danger fw-bold">VISION</h1>
+      <p class="mt-4 fs-3">
+        In a future powered by our app, tricycle rides become effortless. Passengers tap their way to their destination, matched efficiently with nearby drivers for prompt service.
+        We ensure fair fares through transparent calculations, benefitting both riders (confident pricing) and drivers (consistent income).
+        Our commitment extends to the community, partnering with locals to improve the tricycle system and elevate overall well-being.
+        This future of tricycles is not just tech-driven, but deeply rooted in the communities it serves.
+      </p>
+    </div>
+  </div>
+
+  <div class="row d-flex justify-content-center" >
+    <img src="../images/ads.png"  alt="Ads" style="width: 100%; max-width: 800px;">
+  </div>
+</section>
+
+</body>
