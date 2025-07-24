fdc1d9328aa134b892cafce277d4c32ae664496f
Chrischan-Chu <tanchristian2001@gmail.com>
2025-07-24 11:33:58 +0800

    about us section done

---
+++ src/about.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="main.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+
+<body>
+   <section class="d-flex align-items-center text-white" 
+   style="background-image: url('../images/about_background.png'); 
+   background-size: cover; background-position: center; height: 830px;">
+<div class="position-absolute top-0 end-0 mt-3 me-5 fw-bold text-white fs-1">
+      ABOUT US
+    </div>
+        <div class="col-lg-6 ms-5">
+          <h2 class="fw-bold">PTAS: REVOLUTIONIZING RIDES AND REDEFINING THE TRYCICLE INDUSTRY</h2>
+          <p class="mt-4 fs-4">
+            In the ever-evolving landscape of transportation, PTAS emerges as more than just another app. 
+            It shatters the mold of traditional ride-hailing services, offering a revolutionary approach centered around the very people who keep the tricycle industry moving – the drivers. 
+            PTAS transcends the mere act of getting you from point A to point B; it's a catalyst for positive change, empowering drivers, enhancing passenger experiences.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
