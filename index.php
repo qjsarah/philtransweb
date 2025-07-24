cb16bad13e26d705483f8bece4b973c35dfbbdc3
qjsarah <165972652+qjsarah@users.noreply.github.com>
2025-07-24 12:00:47 +0800

    navbar

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+  <meta charset="UTF-8">
+  <title>Manga Info Search</title>
+  <link rel="stylesheet" href="main.css">
+  <script src="bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+</head>
+
+<body>
+    <?php include 'navbar.php'?>
+</body>
+
+</html>
