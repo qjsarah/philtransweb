56fca3fd066c06444e1669706be74b199c029fcd
Jose Favian Reyes <156625483+saeechy@users.noreply.github.com>
2025-07-25 09:09:49 +0800

    unupdated

--- download.php
+++ download.php
@@ -1,6 +1,30 @@
-<link rel="stylesheet" href="main.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="main.css">
+</head>
 <body>
-    <section class="download">
-        <h1>Hi</h1>
-    </section>
+  <section class="download d-flex  ">
+    <div class="mx-5 px-5">
+
+      <h1 class="h1style">DOWNLOAD OUR <span class="text-warning">APP!</span></h1>
+      <p class="textstyle text-dark fs-1 w-75">Download the Philippine Trans App System today and experience 
+        transportation like never before. Whether you're traveling for business 
+        or pleasure, our app makes getting around the Philippines easier, safer,
+        and more convenient than ever before. </p>
+
+        <div class="d-flex gap-5">
+          <img src="imgs/gugil.png" alt="Google Play" class="icon">
+          <img src="imgs/apul.png" alt="App Store" class="icon">
+        </div>
+
+    </div>
+      <div class="person1 flex-row-reverse w-100 h-auto">
+        <img src="imgs/person.png" alt="Person Image" class="img-fluid">
+      </div>
+  </section>
 </body>
+</html>
