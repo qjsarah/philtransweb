cfe7a1670eca9bdf9c37c5af5c03a461059167bc
qjsarah <165972652+qjsarah@users.noreply.github.com>
2025-07-28 16:06:53 +0800

    download mobile

--- public/src/components/download.php
+++ public/src/components/download.php
@@ -4,7 +4,7 @@
 
     <!-- Text & Buttons Column -->
     <div class="h1style mb-4 mb-md-0 px-5">
-      <h1 class="fw-bold text-white display-6 mb-3">DOWNLOAD OUR APP <span class="text-warning">NOW!</span></h1>
+      <h1 class="fw-bold text-white mb-3">DOWNLOAD OUR APP <span class="text-warning">NOW!</span></h1>
       <p class="desktop textstyle text-white mb-4 fs-3">Download the Philippine Trans App System today and experience transportation like never before. Whether you're traveling for business or pleasure, our app makes getting around the Philippines easier, safer, and more convenient than ever before.</p>
 
       <div class="icons d-flex justify-content-center justify-content-md-start gap-5">
