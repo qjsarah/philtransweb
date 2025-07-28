fc6db0f01cd194b6eeb985b721b7c2ee05d562fe
qjsarah <165972652+qjsarah@users.noreply.github.com>
2025-07-28 16:34:17 +0800

    download mobile

--- public/src/components/download.php
+++ public/src/components/download.php
@@ -4,18 +4,18 @@
 
     <!-- Text & Buttons Column -->
     <div class="h1style mb-4 mb-md-0 px-5">
-      <h1 class="fw-bold text-white mb-3">DOWNLOAD OUR APP <span class="text-warning">NOW!</span></h1>
-      <p class="desktop textstyle text-white mb-4 fs-3">Download the Philippine Trans App System today and experience transportation like never before. Whether you're traveling for business or pleasure, our app makes getting around the Philippines easier, safer, and more convenient than ever before.</p>
+      <h1 class="fw-bold text-white mb-3 display-1">DOWNLOAD OUR APP <span class="text-warning">NOW!</span></h1>
+      <p class="desktop textstyle text-white mb-4 fs-3">Download the Philippine Trans App System today and experience transportation like never before. Whether you're traveling for business or pleasure, our app makes getting around the Philippines easier, safer, and more convenient than ever before.</p>  
 
-      <div class="icons d-flex justify-content-center justify-content-md-start gap-5">
-        <img src="../../public/main/images/download_section/apple.png" class="gugil" alt="Google Play" class="img-fluid" style="max-width: 250px;">
-        <img src="../../public/main/images/download_section/google.png" class="img-fluid" alt="App Store" style="max-width: 250px;">
+      <div class="icons d-flex gap-5 flex-md-row flex-sm-row flex-lg-row flex-column justify-content-center justify-content-md-center justify-content-lg-start">
+        <img src="../../public/main/images/download_section/apple.png" class="img-fluid mx-auto mx-lg-0" alt="Google Play"style="max-width: 200px;">
+        <img src="../../public/main/images/download_section/google.png" class="img-fluid mx-auto mx-lg-0" alt="App Store" style="max-width: 200px;">
       </div>
     </div>
 
     <!-- Image Column -->
     <div class=" text-center mt-5 me-5 w-100 mx-auto">
-      <img src="../../public/main/images/download_section/intro_img.png" alt="Person">
+      <img src="../../public/main/images/download_section/intro_img.png" alt="Person" class="img-fluid">
     </div>
       <p class="mobile textstyle text-white mb-4 fs-3 w-75 mx-auto">Download the Philippine Trans App System today and experience transportation like never before. Whether you're traveling for business or pleasure, our app makes getting around the Philippines easier, safer, and more convenient than ever before. </p>
   </div>
